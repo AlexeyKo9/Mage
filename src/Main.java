f2b818669cc1038a012e0a4388690e6a10c83916
Alexey_Ko <tralex_k@mail.ru>
2023-11-12 14:19:13 +0300

    First add

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Mage mage1 = new Mage("Lexa", 12, 150, "ice");
+        Mage mage2 = new Mage("Chich", 10, 100, "fire");
+        Mage mage3 = new Mage("Alena", 12, 150, "ice");
+        System.out.println(mage1.fight(mage3));
+    }
+}
